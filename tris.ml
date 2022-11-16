534ab965f1ba44bbcdfcc82a28a3da62501b4e2a
Soleilhac Bastien <62252156+BobVentron@users.noreply.github.com>
2022-11-16 15:01:11 +0100

    débuts de l'implémentation de la 2éme fonction

--- tris.ml
+++ tris.ml
@@ -15,3 +15,52 @@ let temps_debut = Sys.time () in
   let _ = (* tri de l *) in
   let temps_fin = Sys.time () in
   temps_fin -. temps_debut;;
+  
+(*2éme fonction*)
+(*Fonction de Tie pas sélection du minimum*)
+let min_list ordre l =
+  let rec min_list_aux ordre l a1 a2 =
+    match l with
+      [] -> failwith(" Erreur min_list: il n'y a pas d'élément dans la liste")
+    | [x] -> (x, a2)
+    | x::(y::r) -> if (ordre) x y || x = y
+                   then min_list_aux ordre (x::r) (a1 + 1) a2
+                   else min_list_aux ordre (y::r) (a1 + 1) (a1 +1)
+  in
+  min_list_aux ordre l 0 0;;
+
+(*
+a1 = postion du parcours de la liste
+a2 = position du derniers element minimun trouver 
+ *)
+
+let min_list_indice ordre i l =
+  let rec min_list_indice ordre i l a =
+    match l with
+      [] -> failwith(" Erreur min_list_indice: il n'y a pas d'élément dans la liste")
+    | x::r -> if i = (a - 1)
+              then let (e, indice) = min_list ordre (x::r) in
+                   (e, indice + a)
+              else min_list_indice ordre i r (a + 1)
+  in
+  min_list_indice ordre  i l 0;;
+
+let rec echange e1 i1 e2 i2 l =
+  if i1 = i2
+  then l
+  else match l with
+         [] -> failwith("erreur")
+       | x::r -> if i1 = 0
+                 then e2::(echange e1 (i1 - 1) e2 (i2 - 1) r)
+                 else
+                   if i2 = 0
+                   then e1::r
+                   else x::(echange e1 (i1 - 1) e2 (i2 - 1) r);;
+
+let rec nieme n l =
+  match l with
+    [] -> failwith("erreur")
+  | x::r ->
+     if n = 0
+     then x
+     else nieme (n - 1) r
